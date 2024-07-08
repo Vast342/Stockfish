5d9a666602bceed95efb38198be4173f671decbd
Vast342 <jcpasfield@gmail.com>
2024-07-08 14:17:21 -0400

    Proportional SE, bench: 1188417

--- src/search.cpp
+++ src/search.cpp
@@ -1124,6 +1124,10 @@ moves_loop:  // When in check, search starts here
                 // If we are on a cutNode but the ttMove is not assumed to fail high over current beta (~1 Elo)
                 else if (cutNode)
                     extension = -2;
+
+                // Otherwise, negatively extend proportional to how far off the values are
+                else
+                    extension = (singularBeta - value) / 128;
             }
 
             // Extension for capturing the previous moved piece (~0 Elo on STC, ~1 Elo on LTC)
